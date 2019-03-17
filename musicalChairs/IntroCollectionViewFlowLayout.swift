2821ff5f07699e94f03b4abdf3bc8a23e302a3c0
evapphilips <ep1521@nyu.edu>
2019-03-17 15:20:46 -0400

    layout center collection view and prepare layouts for instructions

---
+++ musicalChairs/IntroCollectionViewFlowLayout.swift
@@ -0,0 +1,13 @@
+//
+//  IntroCollectionViewFlowLayout.swift
+//  musicalChairs
+//
+//  Created by Eva Philips on 3/17/19.
+//  Copyright © 2019 eva&ada. All rights reserved.
+//
+
+import UIKit
+
+class IntroCollectionViewFlowLayout: UICollectionViewFlowLayout {
+
+}
