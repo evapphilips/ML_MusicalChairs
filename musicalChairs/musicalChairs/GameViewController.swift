db9a4b0295818722673f0c2bf8e971940b232684
ada10086 <qj252@nyu.edu>
2019-03-19 16:23:58 -0400

    generated random color for each player, send playerName, player color, (button index) on button tap

--- musicalChairs/musicalChairs/GameViewController.swift
+++ musicalChairs/musicalChairs/GameViewController.swift
@@ -33,16 +33,20 @@ class GameViewController: UIViewController, MultipeerServiceDelegate {
     var multipeerService: MultipeerService?
     var playerName: String?
     
+    //assign at Ready pressed in ReadyViewController
+    let r: CGFloat = .random()
+    let g: CGFloat = .random()
+    let b: CGFloat = .random()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.applyRoundCorner(playerSymbolView)
-        playerSymbolView.layer.backgroundColor = UIColor.red.cgColor
+        playerSymbolView.layer.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1.0).cgColor
         
         self.applyRoundCorner(testButton)
         testButton.layer.borderColor = UIColor.black.cgColor
         testButton.layer.borderWidth = 2
-
+        
         buttons.forEach {
             self.applyRoundCorner($0)
             $0.layer.borderColor = UIColor.black.cgColor
@@ -52,7 +56,7 @@ class GameViewController: UIViewController, MultipeerServiceDelegate {
         if let name = playerName {
             // Start P2P.
             print(name)
-//            self.startMultipeerService(displayName: name)
+            //self.startMultipeerService(displayName: name)
             //playerNameLabel.text = name;
         }
     }
@@ -60,7 +64,7 @@ class GameViewController: UIViewController, MultipeerServiceDelegate {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         restart()
-
+        
     }
     
     @IBAction func handleTestButton(_ sender: Any) {
@@ -72,7 +76,7 @@ class GameViewController: UIViewController, MultipeerServiceDelegate {
     
     @IBAction func buttonsTapped(_ sender: UIButton){
         let index = buttons.index(of: sender)!
-        buttons[index].layer.backgroundColor = UIColor.red.cgColor
+        buttons[index].layer.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1.0).cgColor
         buttons[index].setTitle(playerName, for: .normal)
         buttons[index].setTitleColor(.black, for: .normal)
         buttons.forEach{
@@ -81,28 +85,7 @@ class GameViewController: UIViewController, MultipeerServiceDelegate {
         //get index of button tapped
         print("index:"+"\(index)")
         
-        multipeerService?.send(msg: "\(index)")
-//        multipeerService?.send(msg: playerName!)
-
-//        switch index {
-//        case 0:
-//            sender.layer.backgroundColor = UIColor.red.cgColor
-//            sender.setTitle(playerName, for: .normal)
-//            sender.setTitleColor(.black, for: .normal)
-//            status = true
-//        case 1:
-//            sender.layer.backgroundColor = UIColor.red.cgColor
-//            sender.setTitle(playerName, for: .normal)
-//            sender.setTitleColor(.black, for: .normal)
-//            status = true
-//        case 2:
-//            sender.layer.backgroundColor = UIColor.red.cgColor
-//            sender.setTitle(playerName, for: .normal)
-//            sender.setTitleColor(.black, for: .normal)
-//            status = true
-//        default:
-//            print("nothing")
-//        }
+        multipeerService?.send(msg: "\(index),\(playerName!),\(r),\(g),\(b)")
     }
     
     // Start multipeer service with display name.
@@ -122,10 +105,15 @@ class GameViewController: UIViewController, MultipeerServiceDelegate {
     
     func receivedMsg(manager: MultipeerService, msg: String) {
         DispatchQueue.main.async {
-            if let i = Int(msg) {
-//                self.buttons[i].setTitle(self.playerName, for: .normal)
-                self.buttons[i].layer.backgroundColor = UIColor.red.cgColor
+            let msgArray = msg.components(separatedBy: ",")
+            let name = msgArray[1]
+            if let i = Int(msgArray[0]){
+                self.buttons[i].setTitle(name, for: .normal)
                 self.buttons[i].isEnabled = false
+                guard let rd = NumberFormatter().number(from: msgArray[2]) else {return}
+                guard let gr = NumberFormatter().number(from: msgArray[3]) else {return}
+                guard let bl = NumberFormatter().number(from: msgArray[4]) else {return}
+                self.buttons[i].layer.backgroundColor = UIColor(red: CGFloat(truncating: rd), green: CGFloat(truncating: gr), blue: CGFloat(truncating: bl), alpha: 1.0).cgColor
             }
         }
     }
@@ -171,4 +159,14 @@ class GameViewController: UIViewController, MultipeerServiceDelegate {
         view.endEditing(true)
         return false
     }
+    
 }
+
+extension CGFloat {
+    static func random() -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UInt32.max)
+    }
+}
+
+
+
