4fa8851e33dd58392789a46d2a874f222a1c8355
ada10086 <qj252@nyu.edu>
2019-03-19 13:47:56 -0400

    moved playername input alert to GameViewController as storyboard entrypoint, initialized array of 3 UIButtons all hooked up to 1 IBoutlet & 1 IBAction, implemented disabling buttons on p2p communication

--- musicalChairs/musicalChairs/GameViewController.swift
+++ musicalChairs/musicalChairs/GameViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 var connectedPlayers: Int = 0
 var playerCount: Int = 0
 let playerTotal: Int = 3
+var status: Bool = false;
 
 ////////////////////////////////////////////////////////////////////
 // NOTE: Update to unique name.
@@ -23,33 +24,86 @@ class GameViewController: UIViewController, MultipeerServiceDelegate {
     @IBOutlet weak var testButton: UIButton!
     @IBOutlet weak var playerSymbolView: UIView!
     @IBOutlet weak var playerNameLabel: UILabel!
+    @IBOutlet var buttons : [UIButton]!
     
+    // Popup for entering username.
+    var alert : UIAlertController!
     
     // Service for handling P2P communication.
     var multipeerService: MultipeerService?
     var playerName: String?
     
     
-//    @IBAction func handelTestButton(_ sender: Any) {
-//
-//    }
-
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.applyRoundCorner(playerSymbolView)
+        playerSymbolView.layer.backgroundColor = UIColor.red.cgColor
+        
+        self.applyRoundCorner(testButton)
+        testButton.layer.borderColor = UIColor.black.cgColor
+        testButton.layer.borderWidth = 2
 
+        buttons.forEach {
+            self.applyRoundCorner($0)
+            $0.layer.borderColor = UIColor.black.cgColor
+            $0.layer.borderWidth = 2
+        }
+        
         if let name = playerName {
             // Start P2P.
             print(name)
-            self.startMultipeerService(displayName: name)
-            playerNameLabel.text = name;
+//            self.startMultipeerService(displayName: name)
+            //playerNameLabel.text = name;
         }
-        
-        playerSymbolView.layer.cornerRadius = playerSymbolView.frame.size.width/2
-        playerSymbolView.clipsToBounds = true
-        playerSymbolView.layer.backgroundColor = UIColor.red.cgColor
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        restart()
 
     }
     
+    @IBAction func handleTestButton(_ sender: Any) {
+        testButton.layer.backgroundColor = UIColor.red.cgColor
+        testButton.setTitle(playerName, for: .normal)
+        testButton.setTitleColor(.black, for: .normal)
+    }
+    
+    
+    @IBAction func buttonsTapped(_ sender: UIButton){
+        let index = buttons.index(of: sender)!
+        buttons[index].layer.backgroundColor = UIColor.red.cgColor
+        buttons[index].setTitle(playerName, for: .normal)
+        buttons[index].setTitleColor(.black, for: .normal)
+        buttons.forEach{
+            $0.isEnabled = false
+        }
+        //get index of button tapped
+        print("index:"+"\(index)")
+        
+        multipeerService?.send(msg: "\(index)")
+//        multipeerService?.send(msg: playerName!)
+
+//        switch index {
+//        case 0:
+//            sender.layer.backgroundColor = UIColor.red.cgColor
+//            sender.setTitle(playerName, for: .normal)
+//            sender.setTitleColor(.black, for: .normal)
+//            status = true
+//        case 1:
+//            sender.layer.backgroundColor = UIColor.red.cgColor
+//            sender.setTitle(playerName, for: .normal)
+//            sender.setTitleColor(.black, for: .normal)
+//            status = true
+//        case 2:
+//            sender.layer.backgroundColor = UIColor.red.cgColor
+//            sender.setTitle(playerName, for: .normal)
+//            sender.setTitleColor(.black, for: .normal)
+//            status = true
+//        default:
+//            print("nothing")
+//        }
+    }
     
     // Start multipeer service with display name.
     func startMultipeerService(displayName: String) {
@@ -68,7 +122,53 @@ class GameViewController: UIViewController, MultipeerServiceDelegate {
     
     func receivedMsg(manager: MultipeerService, msg: String) {
         DispatchQueue.main.async {
-            print("hi")
+            if let i = Int(msg) {
+//                self.buttons[i].setTitle(self.playerName, for: .normal)
+                self.buttons[i].layer.backgroundColor = UIColor.red.cgColor
+                self.buttons[i].isEnabled = false
+            }
         }
+    }
+    
+    func applyRoundCorner(_ object:AnyObject){
+        object.layer.cornerRadius = object.frame.size.width/2
+        object.layer?.masksToBounds = true
+    }
+    
+    
+    // Show popup for entering username, P2P servic will start when name entered.
+    func restart() {
+        print("restart")
+        // Create alert popup.
+        alert = UIAlertController(title: "Enter your player name", message: nil, preferredStyle: .alert)
+        alert.addTextField(configurationHandler: { textField in
+            textField.placeholder = "Player name..."
+            textField.addTarget(self, action: #selector(self.alertTextFieldDidChange(_:)), for: .editingChanged)
+        })
+        
+        // Create action on Ready press.
+        let action = UIAlertAction(title: "Ready", style: .default, handler: { action in
+            if let name = self.alert.textFields?.first?.text {
+                self.playerName = name
+                self.playerNameLabel.text = name;
+                self.startMultipeerService(displayName: name)
+            }
+        })
+        action.isEnabled = false
+        alert.addAction(action)
+        
+        // Show alert popup.
+        self.present(alert, animated: true)
+    }
+    
+    // Disable ready button when text field is empty.
+    @objc func alertTextFieldDidChange(_ sender: UITextField) {
+        alert.actions[0].isEnabled = sender.text!.count > 0
+    }
+    
+    // Dismisses keyboard when done is pressed.
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        view.endEditing(true)
+        return false
     }
 }
